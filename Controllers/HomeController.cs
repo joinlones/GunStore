098b4edead105019d92cee3aa1f0306319a502ab
joinlones <88050742+joinlones@users.noreply.github.com>
2024-11-14 11:21:59 +0300

    Add Home

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GunStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // Возвращаем пустое представление
+        }
+
+        public IActionResult Privacy()
+        {
+            return View(); // Возвращаем представление для страницы конфиденциальности
+        }
+    }
+}
